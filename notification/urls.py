c00007e3dbbf2d3d1c2481ec765aa86101fdcd0e
Bryan Fernandez <bryfern710@gmail.com>
2020-03-06 15:33:36 -0600

    changed tweet, and notification to classes

--- notification/urls.py
+++ notification/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from notification import views
 
 urlpatterns = [
-    path('notification/', views.notification_view, name="notifications"),
-    path('cleannotify/', views.cleared_notifications)
+    path('notification/', views.NotificationsView.as_view(), name="notifications"),
+    path('cleannotify/', views.ClearNotification.as_view())
 ]
--- notification/views.py
+++ notification/views.py
@@ -1,25 +1,27 @@
 from django.shortcuts import render, reverse, HttpResponseRedirect
 from notification.models import Notification
-# Create your views here.
+from django.views import View
 
 
-def notification_view(request):
-    html = "notification.html"
+class NotificationsView(View):
+    def get(self, request):
+        html = "notification.html"
 
-    notifications = Notification.objects.filter(target_user=request.user).filter(seen=False).all().order_by('-date_filed')
-    notification_count = notifications.count()
-    return render(request, html,
-                  {
-                      "notifications": notifications,
-                      "notify_count": notification_count
-                  })
+        notifications = Notification.objects.filter(target_user=request.user).filter(seen=False).all().order_by('-date_filed')
+        notification_count = notifications.count()
+        return render(request, html,
+                      {
+                        "notifications": notifications,
+                        "notify_count": notification_count
+                      })
 
 
-def cleared_notifications(request):
-    notifications = Notification.objects.filter(target_user=request.user).all()
+class ClearNotification(View):
+    def get(request):
+        notifications = Notification.objects.filter(target_user=request.user).all()
 
-    for each in notifications:
-        each.seen = True
-        each.save()
+        for each in notifications:
+            each.seen = True
+            each.save()
 
-    return HttpResponseRedirect(reverse("notifications"))
+        return HttpResponseRedirect(reverse("notifications"))
