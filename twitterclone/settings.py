c944e037cb72c691f2c9af3c5e9d069c47d0d3c8
Bryan Fernandez <bryfern710@gmail.com>
2020-04-13 22:53:35 -0500

    added pip installs

--- twitterclone/settings.py
+++ twitterclone/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l!0bh6&z#qy@x^ifrf67_fsg824#z0@58i_h%!xa!xj!^0a%dd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['twitter-clone-42.herokuapp.com']
+ALLOWED_HOSTS = ['twitter-clone-42.herokuapp.com', '127.0.0.1:8000', 'localhost']
 
 
 # Application definition
